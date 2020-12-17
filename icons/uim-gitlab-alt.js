748958ecf948cb3663b54c1c5df8f99c31c05840
Tarun Mangukiya <tarunmangukiya@hotmail.com>
2020-12-17 11:46:45 +0530

    release: 0.0.5 (#20)
    * release: 0.0.5
    
    * [create-pull-request] automated change
    
    Co-authored-by: tarunmangukiya <tarunmangukiya@users.noreply.github.com>

---
+++ icons/uim-gitlab-alt.js
@@ -0,0 +1,38 @@
+import * as React from "react";
+import "../utils/style.css";
+
+function UimGitlabAlt(props) {
+  return /*#__PURE__*/React.createElement("svg", Object.assign({}, props, {
+    "data-name": "Layer 1",
+    viewBox: "0 0 24 24",
+    width: props.size || '1em',
+    height: props.size || '1em',
+    fill: "currentColor",
+    className: `ui-svg-inline ${props.className || ''}`
+  }), /*#__PURE__*/React.createElement("g", {
+    "data-name": "logo art"
+  }, /*#__PURE__*/React.createElement("path", {
+    opacity: 1,
+    d: "M11.99935,21.42092h0l3.68451-11.33344H8.32017l3.67918,11.33344Z"
+  }), /*#__PURE__*/React.createElement("path", {
+    opacity: 0.25,
+    d: "M3.1594,10.08748h0l-1.122,3.44324a.76313.76313,0,0,0,.27611.85267l9.68586,7.03747L3.1594,10.08748Z"
+  }), /*#__PURE__*/React.createElement("path", {
+    opacity: 1,
+    d: "M3.1594,10.08748H8.32017L6.09912,3.2619a.3831.3831,0,0,0-.72685,0L3.1594,10.08748Z"
+  }), /*#__PURE__*/React.createElement("path", {
+    opacity: 0.25,
+    d: "M20.84463,10.08748h0l1.118,3.44324a.76313.76313,0,0,1-.27611.85267l-9.68716,7.03753,8.84528-11.33344Z"
+  }), /*#__PURE__*/React.createElement("path", {
+    opacity: 1,
+    d: "M20.84463,10.08748H15.68386l2.217-6.82558a.3831.3831,0,0,1,.72685,0l2.217,6.82558Z"
+  }), /*#__PURE__*/React.createElement("polygon", {
+    opacity: 0.5,
+    points: "11.999 21.421 15.684 10.087 20.845 10.087 11.999 21.421"
+  }), /*#__PURE__*/React.createElement("polygon", {
+    opacity: 0.5,
+    points: "11.999 21.421 3.159 10.087 8.32 10.087 11.999 21.421"
+  })));
+}
+
+export default UimGitlabAlt;
---
+++ icons/uim-lottiefiles.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import "../utils/style.css";
+
+function UimLottiefiles(props) {
+  return /*#__PURE__*/React.createElement("svg", Object.assign({}, props, {
+    "data-name": "Layer 1",
+    viewBox: "0 0 24 24",
+    width: props.size || '1em',
+    height: props.size || '1em',
+    fill: "currentColor",
+    className: `ui-svg-inline ${props.className || ''}`
+  }), /*#__PURE__*/React.createElement("path", {
+    opacity: 0.5,
+    d: "M7,18a1,1,0,0,1,0-2c1.65918,0,2.85645-2.17725,4.124-4.48193C12.61621,8.80518,14.15918,6,17,6a1,1,0,0,1,0,2c-1.65918,0-2.85645,2.17725-4.124,4.48193C11.38379,15.19482,9.84082,18,7,18Z"
+  }), /*#__PURE__*/React.createElement("path", {
+    opacity: 1,
+    d: "M19,2H5A3.00328,3.00328,0,0,0,2,5V19a3.00328,3.00328,0,0,0,3,3H19a3.00328,3.00328,0,0,0,3-3V5A3.00328,3.00328,0,0,0,19,2ZM17,8c-1.65918,0-2.85645,2.17725-4.124,4.48193C11.38379,15.19482,9.84082,18,7,18a1,1,0,0,1,0-2c1.65918,0,2.85645-2.17725,4.124-4.48193C12.61621,8.80518,14.15918,6,17,6a1,1,0,0,1,0,2Z"
+  }));
+}
+
+export default UimLottiefiles;
